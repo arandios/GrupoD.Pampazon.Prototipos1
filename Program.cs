f28419f7f7abd2eeff02c320b74e63b4104a17bf
lucasburgosm <75702951+lucasburgosm@users.noreply.github.com>
2024-10-02 21:14:08 -0300

    #testAgregar archivos de proyecto.

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Pampazon
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Generar_RemitoForms());
+            Application.Run(new Generar_RemitoForms()); // comentario test //
         }
     }
 }
