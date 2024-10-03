c898f161c90da499f6cf97ec4b231c23913e1738
TOMAS IGNACIO SAMPIETRO <08SA42645963@campus.economicas.uba.ar>
2024-10-03 07:10:49 -0300

    Agregar opciones al Menu Principal

--- MenuPrincipal/MenuPrincipal.Designer.cs
+++ MenuPrincipal/MenuPrincipal.Designer.cs
@@ -48,56 +48,56 @@
             // 
             // button1
             // 
-            button1.Location = new Point(304, 94);
+            button1.Location = new Point(256, 97);
             button1.Name = "button1";
-            button1.Size = new Size(190, 38);
+            button1.Size = new Size(271, 38);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "1. Registrar Órden de Preparación";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(304, 226);
+            button2.Location = new Point(256, 229);
             button2.Name = "button2";
-            button2.Size = new Size(190, 38);
+            button2.Size = new Size(271, 38);
             button2.TabIndex = 2;
-            button2.Text = "button2";
+            button2.Text = "Confirmar Órden de Selección";
             button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new Point(304, 138);
+            button3.Location = new Point(256, 141);
             button3.Name = "button3";
-            button3.Size = new Size(190, 38);
+            button3.Size = new Size(271, 38);
             button3.TabIndex = 3;
-            button3.Text = "button3";
+            button3.Text = "2. Generar Órden de Selección";
             button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            button4.Location = new Point(304, 182);
+            button4.Location = new Point(256, 185);
             button4.Name = "button4";
-            button4.Size = new Size(190, 38);
+            button4.Size = new Size(271, 38);
             button4.TabIndex = 4;
             button4.Text = "button4";
             button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            button5.Location = new Point(304, 270);
+            button5.Location = new Point(256, 273);
             button5.Name = "button5";
-            button5.Size = new Size(190, 38);
+            button5.Size = new Size(271, 38);
             button5.TabIndex = 5;
-            button5.Text = "button5";
+            button5.Text = "Generar Órden de Entrega";
             button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            button6.Location = new Point(304, 314);
+            button6.Location = new Point(256, 317);
             button6.Name = "button6";
-            button6.Size = new Size(190, 38);
+            button6.Size = new Size(271, 38);
             button6.TabIndex = 6;
-            button6.Text = "button6";
+            button6.Text = "Generar Remito";
             button6.UseVisualStyleBackColor = true;
             // 
             // MenuPrincipal
