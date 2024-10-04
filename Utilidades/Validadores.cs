047e202b73e46fdb517af3fb9a49b557f0d54a82
lorena mazzalupo <lorenamazzalupo@gmail.com>
2024-10-04 15:57:10 -0300

    # Validadores de int y float, falta string

---
+++ Utilidades/Validadores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pampazon.Utilidades
+{
+      static class Validadores
+    {
+        public static bool esNumeroInt(string numeroStr)
+        {
+            int n;
+            return int.TryParse(numeroStr, out n);
+        }
+        public static int obtenerNumeroInt(string numeroStr)
+        {
+            int n;
+            bool esInt =  int.TryParse(numeroStr, out n);
+            if (esInt) {
+                return n;
+            } else { return -1; }
+        }
+        public static bool esNumeroFloat(string numeroStr)
+        {
+            int n;
+            return int.TryParse(numeroStr, out n);
+        }
+        public static int obtenerNumeroFloat(string numeroStr)
+        {
+            int n;
+            bool esInt = int.TryParse(numeroStr, out n);
+            if (esInt)
+            {
+                return n;
+            }
+            else { return -1; }
+        }
+    }
+
+}
