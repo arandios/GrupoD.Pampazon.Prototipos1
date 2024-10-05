793e8aed82d621f1483cde94e77667a357abd7d3
AlanPrado03 <pradoalan47@gmail.com>
2024-10-05 19:58:03 -0300

    Ordenes entrega

--- OrdenEntrega/OrdenEntrega.Designer.cs
+++ OrdenEntrega/OrdenEntrega.Designer.cs
@@ -41,27 +41,24 @@
             columnHeader7 = new ColumnHeader();
             DetalleOrdenbtn = new Button();
             listView2 = new ListView();
-            columnHeader1 = new ColumnHeader();
-            columnHeader2 = new ColumnHeader();
-            columnHeader3 = new ColumnHeader();
-            columnHeader8 = new ColumnHeader();
-            columnHeader9 = new ColumnHeader();
             SuspendLayout();
             // 
             // Buscarbtn
             // 
-            Buscarbtn.Location = new Point(721, 83);
+            Buscarbtn.Location = new Point(505, 50);
+            Buscarbtn.Margin = new Padding(2, 2, 2, 2);
             Buscarbtn.Name = "Buscarbtn";
-            Buscarbtn.Size = new Size(136, 38);
+            Buscarbtn.Size = new Size(95, 23);
             Buscarbtn.TabIndex = 18;
             Buscarbtn.Text = "Buscar";
             Buscarbtn.UseVisualStyleBackColor = true;
             // 
             // Salirbtn
             // 
-            Salirbtn.Location = new Point(990, 605);
+            Salirbtn.Location = new Point(693, 363);
+            Salirbtn.Margin = new Padding(2, 2, 2, 2);
             Salirbtn.Name = "Salirbtn";
-            Salirbtn.Size = new Size(221, 72);
+            Salirbtn.Size = new Size(155, 43);
             Salirbtn.TabIndex = 17;
             Salirbtn.Text = "salir";
             Salirbtn.UseVisualStyleBackColor = true;
@@ -69,9 +66,10 @@
             // 
             // GenerarOrdenEntregabtn
             // 
-            GenerarOrdenEntregabtn.Location = new Point(990, 520);
+            GenerarOrdenEntregabtn.Location = new Point(693, 312);
+            GenerarOrdenEntregabtn.Margin = new Padding(2, 2, 2, 2);
             GenerarOrdenEntregabtn.Name = "GenerarOrdenEntregabtn";
-            GenerarOrdenEntregabtn.Size = new Size(221, 78);
+            GenerarOrdenEntregabtn.Size = new Size(155, 47);
             GenerarOrdenEntregabtn.TabIndex = 16;
             GenerarOrdenEntregabtn.Text = "Generar Orden Entrega";
             GenerarOrdenEntregabtn.UseVisualStyleBackColor = true;
@@ -79,34 +77,38 @@
             // FechaEntregacmb
             // 
             FechaEntregacmb.FormattingEnabled = true;
-            FechaEntregacmb.Location = new Point(400, 83);
+            FechaEntregacmb.Location = new Point(280, 50);
+            FechaEntregacmb.Margin = new Padding(2, 2, 2, 2);
             FechaEntregacmb.Name = "FechaEntregacmb";
-            FechaEntregacmb.Size = new Size(183, 33);
+            FechaEntregacmb.Size = new Size(129, 23);
             FechaEntregacmb.TabIndex = 15;
             FechaEntregacmb.Text = "Fecha entrega";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(90, 88);
+            label1.Location = new Point(63, 53);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(85, 25);
+            label1.Size = new Size(54, 15);
             label1.TabIndex = 13;
             label1.Text = "ID Orden";
             // 
             // txtIdOrden
             // 
-            txtIdOrden.Location = new Point(173, 83);
+            txtIdOrden.Location = new Point(121, 50);
+            txtIdOrden.Margin = new Padding(2, 2, 2, 2);
             txtIdOrden.Name = "txtIdOrden";
-            txtIdOrden.Size = new Size(150, 31);
+            txtIdOrden.Size = new Size(106, 23);
             txtIdOrden.TabIndex = 12;
             txtIdOrden.TextChanged += textBox1_TextChanged;
             // 
             // limpiarbtn
             // 
-            limpiarbtn.Location = new Point(863, 83);
+            limpiarbtn.Location = new Point(604, 50);
+            limpiarbtn.Margin = new Padding(2, 2, 2, 2);
             limpiarbtn.Name = "limpiarbtn";
-            limpiarbtn.Size = new Size(136, 38);
+            limpiarbtn.Size = new Size(95, 23);
             limpiarbtn.TabIndex = 11;
             limpiarbtn.Text = "Limpiar";
             limpiarbtn.UseVisualStyleBackColor = true;
@@ -114,9 +116,10 @@
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { columnHeader5, columnHeader6, columnHeader7 });
-            listView1.Location = new Point(911, 155);
+            listView1.Location = new Point(638, 93);
+            listView1.Margin = new Padding(2, 2, 2, 2);
             listView1.Name = "listView1";
-            listView1.Size = new Size(341, 354);
+            listView1.Size = new Size(240, 214);
             listView1.TabIndex = 19;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
@@ -139,59 +142,29 @@
             // 
             // DetalleOrdenbtn
             // 
-            DetalleOrdenbtn.Location = new Point(59, 520);
+            DetalleOrdenbtn.Location = new Point(41, 312);
+            DetalleOrdenbtn.Margin = new Padding(2, 2, 2, 2);
             DetalleOrdenbtn.Name = "DetalleOrdenbtn";
-            DetalleOrdenbtn.Size = new Size(221, 63);
+            DetalleOrdenbtn.Size = new Size(155, 38);
             DetalleOrdenbtn.TabIndex = 20;
             DetalleOrdenbtn.Text = "Detalles de Orden";
             DetalleOrdenbtn.UseVisualStyleBackColor = true;
             // 
             // listView2
             // 
-            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader8, columnHeader9 });
-            listView2.Location = new Point(59, 155);
-            listView2.Margin = new Padding(4, 5, 4, 5);
+            listView2.Location = new Point(41, 93);
             listView2.Name = "listView2";
-            listView2.Size = new Size(817, 354);
+            listView2.Size = new Size(573, 214);
             listView2.TabIndex = 21;
             listView2.UseCompatibleStateImageBehavior = false;
             listView2.View = View.Details;
             listView2.SelectedIndexChanged += listView2_SelectedIndexChanged;
             // 
-            // columnHeader1
-            // 
-            columnHeader1.Text = "Nro orden";
-            columnHeader1.Width = 100;
-            // 
-            // columnHeader2
-            // 
-            columnHeader2.DisplayIndex = 4;
-            columnHeader2.Text = "Estado";
-            columnHeader2.Width = 100;
-            // 
-            // columnHeader3
-            // 
-            columnHeader3.DisplayIndex = 1;
-            columnHeader3.Text = "Fecha de entrega";
-            columnHeader3.Width = 120;
-            // 
-            // columnHeader8
-            // 
-            columnHeader8.DisplayIndex = 2;
-            columnHeader8.Text = "Id Transportista";
-            columnHeader8.Width = 95;
-            // 
-            // columnHeader9
-            // 
-            columnHeader9.DisplayIndex = 3;
-            columnHeader9.Text = "Nombre Transportista";
-            columnHeader9.Width = 140;
-            // 
             // OrdenEntrega
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1284, 752);
+            ClientSize = new Size(899, 451);
             Controls.Add(listView2);
             Controls.Add(DetalleOrdenbtn);
             Controls.Add(listView1);
@@ -202,6 +175,7 @@
             Controls.Add(label1);
             Controls.Add(txtIdOrden);
             Controls.Add(limpiarbtn);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "OrdenEntrega";
             Text = "Orden de entrega";
             ResumeLayout(false);
@@ -220,13 +194,8 @@
         private ListView listView1;
         private Button DetalleOrdenbtn;
         private ListView listView2;
-        private ColumnHeader columnHeader1;
-        private ColumnHeader columnHeader2;
-        private ColumnHeader columnHeader3;
         private ColumnHeader columnHeader5;
         private ColumnHeader columnHeader6;
         private ColumnHeader columnHeader7;
-        private ColumnHeader columnHeader8;
-        private ColumnHeader columnHeader9;
     }
 }
--- OrdenEntrega/OrdenEntrega.cs
+++ OrdenEntrega/OrdenEntrega.cs
@@ -12,12 +12,19 @@ namespace Pampazon.OrdenEntrega
 {
     public partial class OrdenEntrega : Form
     {
+        public class OrdenEntregaData
+        {
+            public int NroOrden { get; set; }
+            public string FechaEntrega { get; set; }
+            public int IdTransportista { get; set; }
+            public string NombreTransportista { get; set; }
+            public string Estado { get; set; }
+        }
         public OrdenEntrega()
         {
             InitializeComponent();
             ConfigurarListView();
             AgregarDatos();
-            AgregarDatos2();
             AgregarDatosl3();
             AgregarDatosl2();
         }
@@ -25,32 +32,43 @@ namespace Pampazon.OrdenEntrega
         {
             listView2.View = View.Details;
             listView2.FullRowSelect = true;
-         
-            listView1.View = View.Details;
-            listView1.FullRowSelect = true;
-           
+
+            listView2.Columns.Add("Nro Orden", -2, HorizontalAlignment.Left);
+            listView2.Columns.Add("Fecha de Entrega", -2, HorizontalAlignment.Left);
+            listView2.Columns.Add("ID Transportista", -2, HorizontalAlignment.Left);
+            listView2.Columns.Add("Nombre Transportista", -2, HorizontalAlignment.Left);
+            listView2.Columns.Add("Estado", -2, HorizontalAlignment.Left);       
         }
+
         private void AgregarDatos()
         {
+            List<OrdenEntregaData> datos = new List<OrdenEntregaData>();
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                datos.Add(new OrdenEntregaData
+                {
+                    NroOrden = i*2,
+                    FechaEntrega = "2024-10-0"+i,
+                    IdTransportista = i,
+                    NombreTransportista = "cliente " + i,
+                    Estado = "Entregado"
 
-            ListViewItem item = new ListViewItem("12345");
-            item.SubItems.Add("Entregado");
-            item.SubItems.Add("987");
-            item.SubItems.Add("2024-10-05");
-            item.SubItems.Add("Juan Pérez");
-            listView2.Items.Add(item);
+                });
+            }
+            foreach (var dato in datos)
+            {
+                ListViewItem item = new ListViewItem(dato.NroOrden.ToString());
+                item.SubItems.Add(dato.FechaEntrega);
+                item.SubItems.Add(dato.IdTransportista.ToString());
+                item.SubItems.Add(dato.NombreTransportista);
+                item.SubItems.Add(dato.Estado);
+                listView2.Items.Add(item);
+            }
+
+
         }
-        private void AgregarDatos2()
-        {
-            ListViewItem item = new ListViewItem("12346");
-            item.SubItems.Add("Entregado"); // Cambiado el orden de los subitems
-            item.SubItems.Add("988");
-            item.SubItems.Add("2024-10-06");
-            item.SubItems.Add("Juan Domingo Perón");
-            listView2.Items.Add(item);
-        }
-       
-        
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -63,7 +81,7 @@ namespace Pampazon.OrdenEntrega
 
         private void listView2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void AgregarDatosl2()
