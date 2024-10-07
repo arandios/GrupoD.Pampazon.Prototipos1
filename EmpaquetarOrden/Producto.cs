c750e5cb78fee124b6013ab2af044c7380c8808b
lorena mazzalupo <lorenamazzalupo@gmail.com>
2024-10-07 17:15:10 -0300

    Clases

---
+++ EmpaquetarOrden/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pampazon.EmpaquetarOrden
+{
+    internal class Producto
+    {
+        public int IdProducto { get; set; }
+        public string DescripcionProducto { get; set; }
+        public string Detalle { get; set; }
+    }
+}
