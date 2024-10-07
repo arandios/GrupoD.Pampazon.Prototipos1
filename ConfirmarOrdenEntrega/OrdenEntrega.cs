abee71558123cebfc7a65855198731e3f3296634
DANIEL ANGEL CLAUDIO PONCE <47CL96096669@campus.economicas.uba.ar>
2024-10-07 17:29:25 -0300

    dd

---
+++ ConfirmarOrdenEntrega/OrdenEntrega.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pampazon.ConfirmarOrdenEntrega
+{
+    internal class OrdenEntrega
+    {
+
+        public int Nro_OrdenP { get; set; }
+        public DateTime FechaEmision { get; set; }
+        OP_asociada
+        public string Estado { get; set; }
+        public DateTime Fecha_Estado { get; set; }
+        
+
+
+        public OrdenEntrega(int nro_orden, DateTime fechaemision, List<int> op_asociadas, string estado, DateTime fecha_estado)
+        {
+            Nro_OrdenP = nro_orden;
+            FechaEmision = fechaemision;
+            
+            Estado = estado;
+            Fecha_Estado = fecha_estado;
+            
+        }
+    }
+}
