cc191f27947f520ad39de4c368b3114df0b98d93
EZEQUIEL MARCELO ARANDA <65AR43000399@campus.economicas.uba.ar>
2024-10-10 23:16:08 -0300

    c

--- OrdenEntrega/OrdenEntregaForm.Designer.cs
+++ OrdenEntrega/OrdenEntregaForm.Designer.cs
@@ -41,11 +41,18 @@
             label3 = new Label();
             Seleccionar = new Button();
             Eliminar = new Button();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // Buscarbtn
             // 
-            Buscarbtn.Location = new Point(339, 33);
+            Buscarbtn.Location = new Point(102, 81);
             Buscarbtn.Margin = new Padding(2);
             Buscarbtn.Name = "Buscarbtn";
             Buscarbtn.Size = new Size(95, 23);
@@ -56,21 +63,21 @@
             // 
             // Salirbtn
             // 
-            Salirbtn.Location = new Point(679, 363);
+            Salirbtn.Location = new Point(274, 228);
             Salirbtn.Margin = new Padding(2);
             Salirbtn.Name = "Salirbtn";
-            Salirbtn.Size = new Size(155, 43);
+            Salirbtn.Size = new Size(89, 35);
             Salirbtn.TabIndex = 17;
-            Salirbtn.Text = "salir";
+            Salirbtn.Text = "Salir";
             Salirbtn.UseVisualStyleBackColor = true;
             Salirbtn.Click += Salirbtn_Click;
             // 
             // GenerarOrdenEntregabtn
             // 
-            GenerarOrdenEntregabtn.Location = new Point(679, 312);
+            GenerarOrdenEntregabtn.Location = new Point(99, 228);
             GenerarOrdenEntregabtn.Margin = new Padding(2);
             GenerarOrdenEntregabtn.Name = "GenerarOrdenEntregabtn";
-            GenerarOrdenEntregabtn.Size = new Size(155, 47);
+            GenerarOrdenEntregabtn.Size = new Size(171, 35);
             GenerarOrdenEntregabtn.TabIndex = 16;
             GenerarOrdenEntregabtn.Text = "Generar Orden Entrega";
             GenerarOrdenEntregabtn.UseVisualStyleBackColor = true;
@@ -80,10 +87,10 @@
             // 
             FechaEntregacmb.FormattingEnabled = true;
             FechaEntregacmb.Items.AddRange(new object[] { "2024-10-01", "2024-10-02", "2024-10-03" });
-            FechaEntregacmb.Location = new Point(200, 33);
+            FechaEntregacmb.Location = new Point(102, 48);
             FechaEntregacmb.Margin = new Padding(2);
             FechaEntregacmb.Name = "FechaEntregacmb";
-            FechaEntregacmb.Size = new Size(129, 23);
+            FechaEntregacmb.Size = new Size(207, 23);
             FechaEntregacmb.TabIndex = 15;
             FechaEntregacmb.Text = "Fecha entrega";
             FechaEntregacmb.SelectedIndexChanged += FechaEntregacmb_SelectedIndexChanged;
@@ -91,7 +98,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(11, 36);
+            label1.Location = new Point(23, 24);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
             label1.Size = new Size(54, 15);
@@ -100,16 +107,16 @@
             // 
             // txtIdOrden
             // 
-            txtIdOrden.Location = new Point(90, 33);
+            txtIdOrden.Location = new Point(102, 21);
             txtIdOrden.Margin = new Padding(2);
             txtIdOrden.Name = "txtIdOrden";
-            txtIdOrden.Size = new Size(106, 23);
+            txtIdOrden.Size = new Size(207, 23);
             txtIdOrden.TabIndex = 12;
             txtIdOrden.TextChanged += txtIdOrden_TextChanged;
             // 
             // limpiarbtn
             // 
-            limpiarbtn.Location = new Point(339, 65);
+            limpiarbtn.Location = new Point(214, 81);
             limpiarbtn.Margin = new Padding(2);
             limpiarbtn.Name = "limpiarbtn";
             limpiarbtn.Size = new Size(95, 23);
@@ -120,34 +127,34 @@
             // 
             // Ordenes_Preparacion
             // 
-            Ordenes_Preparacion.Location = new Point(12, 93);
+            Ordenes_Preparacion.Location = new Point(3, 22);
             Ordenes_Preparacion.Name = "Ordenes_Preparacion";
-            Ordenes_Preparacion.Size = new Size(422, 214);
+            Ordenes_Preparacion.Size = new Size(358, 169);
             Ordenes_Preparacion.TabIndex = 21;
             Ordenes_Preparacion.UseCompatibleStateImageBehavior = false;
             Ordenes_Preparacion.View = View.Details;
             // 
             // Orden_Entrega
             // 
-            Orden_Entrega.Location = new Point(470, 93);
+            Orden_Entrega.Location = new Point(6, 26);
             Orden_Entrega.Name = "Orden_Entrega";
-            Orden_Entrega.Size = new Size(364, 214);
+            Orden_Entrega.Size = new Size(357, 196);
             Orden_Entrega.TabIndex = 22;
             Orden_Entrega.UseCompatibleStateImageBehavior = false;
+            //Orden_Entrega.SelectedIndexChanged += Orden_Entrega_SelectedIndexChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 75);
+            label2.Location = new Point(12, 164);
             label2.Name = "label2";
-            label2.Size = new Size(133, 15);
+            label2.Size = new Size(0, 15);
             label2.TabIndex = 23;
-            label2.Text = "Ordenes de Preparación";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(470, 75);
+            label3.Location = new Point(19, 394);
             label3.Name = "label3";
             label3.Size = new Size(120, 15);
             label3.TabIndex = 24;
@@ -155,9 +162,9 @@
             // 
             // Seleccionar
             // 
-            Seleccionar.Location = new Point(349, 312);
+            Seleccionar.Location = new Point(175, 197);
             Seleccionar.Name = "Seleccionar";
-            Seleccionar.Size = new Size(85, 47);
+            Seleccionar.Size = new Size(186, 35);
             Seleccionar.TabIndex = 25;
             Seleccionar.Text = "Seleccionar";
             Seleccionar.UseVisualStyleBackColor = true;
@@ -165,36 +172,80 @@
             // 
             // Eliminar
             // 
-            Eliminar.Location = new Point(470, 313);
+            Eliminar.Location = new Point(5, 228);
             Eliminar.Name = "Eliminar";
-            Eliminar.Size = new Size(89, 46);
+            Eliminar.Size = new Size(89, 35);
             Eliminar.TabIndex = 26;
             Eliminar.Text = "Eliminar";
             Eliminar.UseVisualStyleBackColor = true;
             Eliminar.Click += Eliminar_Click;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 56);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 27;
+            label4.Text = "Fecha";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtIdOrden);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(limpiarbtn);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(FechaEntregacmb);
+            groupBox1.Controls.Add(Buscarbtn);
+            groupBox1.Location = new Point(11, 7);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(366, 126);
+            groupBox1.TabIndex = 28;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Busqueda de ordenes";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(Ordenes_Preparacion);
+            groupBox2.Controls.Add(Seleccionar);
+            groupBox2.Location = new Point(8, 139);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(369, 239);
+            groupBox2.TabIndex = 29;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Ordenes de Preparación";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(Orden_Entrega);
+            groupBox3.Controls.Add(GenerarOrdenEntregabtn);
+            groupBox3.Controls.Add(Eliminar);
+            groupBox3.Controls.Add(Salirbtn);
+            groupBox3.Location = new Point(8, 384);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(369, 265);
+            groupBox3.TabIndex = 26;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Ordenes de Entrega";
+            // 
             // OrdenEntregaForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(846, 417);
-            Controls.Add(Eliminar);
-            Controls.Add(Seleccionar);
+            ClientSize = new Size(388, 661);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(Orden_Entrega);
-            Controls.Add(Ordenes_Preparacion);
-            Controls.Add(Buscarbtn);
-            Controls.Add(Salirbtn);
-            Controls.Add(GenerarOrdenEntregabtn);
-            Controls.Add(FechaEntregacmb);
-            Controls.Add(label1);
-            Controls.Add(txtIdOrden);
-            Controls.Add(limpiarbtn);
             Margin = new Padding(2);
             Name = "OrdenEntregaForm";
             Text = "Orden de entrega";
             Load += OrdenEntregaForm_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -214,5 +265,9 @@
         private Label label3;
         private Button Seleccionar;
         private Button Eliminar;
+        private Label label4;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
     }
 }
