a994f191d14df9b7299725e70760327552e7aa75
Tomas Ignacio Sampietro <109176221+tomisampietro@users.noreply.github.com>
2024-10-12 19:41:14 -0300

    Add files via upload

---
+++ 5. GenerarOrdenEntrega/GenerarOrdenEntregaForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Pampazon.OrdenEntrega
+{
+    partial class GenerarOrdenEntregaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Salirbtn = new Button();
+            GenerarOrdenEntregabtn = new Button();
+            label2 = new Label();
+            groupBox3 = new GroupBox();
+            Orden_Entrega = new ListView();
+            CodOrdenColumna = new ColumnHeader();
+            RazonClienteColumna = new ColumnHeader();
+            TransportistaColumna = new ColumnHeader();
+            fechaOPColumna = new ColumnHeader();
+            fechaOEColumna = new ColumnHeader();
+            OPColumna = new ColumnHeader();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Salirbtn
+            // 
+            Salirbtn.Location = new Point(855, 275);
+            Salirbtn.Margin = new Padding(2, 2, 2, 2);
+            Salirbtn.Name = "Salirbtn";
+            Salirbtn.Size = new Size(117, 38);
+            Salirbtn.TabIndex = 17;
+            Salirbtn.Text = "&Salir";
+            Salirbtn.UseVisualStyleBackColor = true;
+            Salirbtn.Click += Salirbtn_Click;
+            // 
+            // GenerarOrdenEntregabtn
+            // 
+            GenerarOrdenEntregabtn.BackColor = Color.FromArgb(192, 255, 192);
+            GenerarOrdenEntregabtn.Location = new Point(595, 274);
+            GenerarOrdenEntregabtn.Margin = new Padding(2, 2, 2, 2);
+            GenerarOrdenEntregabtn.Name = "GenerarOrdenEntregabtn";
+            GenerarOrdenEntregabtn.Size = new Size(256, 38);
+            GenerarOrdenEntregabtn.TabIndex = 16;
+            GenerarOrdenEntregabtn.Text = "&Generar/ Confirmar Orden de Entrega";
+            GenerarOrdenEntregabtn.UseVisualStyleBackColor = false;
+            GenerarOrdenEntregabtn.Click += GenerarOrdenEntregabtn_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 23;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(Orden_Entrega);
+            groupBox3.Controls.Add(GenerarOrdenEntregabtn);
+            groupBox3.Controls.Add(Salirbtn);
+            groupBox3.Location = new Point(8, 19);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(989, 318);
+            groupBox3.TabIndex = 26;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Ordenes a despacho:";
+            // 
+            // Orden_Entrega
+            // 
+            Orden_Entrega.Columns.AddRange(new ColumnHeader[] { CodOrdenColumna, RazonClienteColumna, TransportistaColumna, fechaOPColumna, fechaOEColumna, OPColumna });
+            Orden_Entrega.Location = new Point(6, 22);
+            Orden_Entrega.Name = "Orden_Entrega";
+            Orden_Entrega.Size = new Size(966, 248);
+            Orden_Entrega.TabIndex = 22;
+            Orden_Entrega.UseCompatibleStateImageBehavior = false;
+            Orden_Entrega.View = View.Details;
+            // 
+            // CodOrdenColumna
+            // 
+            CodOrdenColumna.Text = "Código Orden de Entrega";
+            CodOrdenColumna.Width = 160;
+            // 
+            // RazonClienteColumna
+            // 
+            RazonClienteColumna.Text = "Razón Social";
+            RazonClienteColumna.Width = 160;
+            // 
+            // TransportistaColumna
+            // 
+            TransportistaColumna.Text = "Transportista";
+            TransportistaColumna.Width = 160;
+            // 
+            // fechaOPColumna
+            // 
+            fechaOPColumna.Text = "Fecha Orden de Preparación";
+            fechaOPColumna.Width = 160;
+            // 
+            // fechaOEColumna
+            // 
+            fechaOEColumna.Text = "Fecha Orden de Entrega";
+            fechaOEColumna.Width = 160;
+            // 
+            // OPColumna
+            // 
+            OPColumna.Text = "Orden Preparación asociada";
+            OPColumna.Width = 160;
+            // 
+            // GenerarOrdenEntregaForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1013, 346);
+            Controls.Add(groupBox3);
+            Controls.Add(label2);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "GenerarOrdenEntregaForm";
+            Text = "Generar Orden de Entrega";
+            Load += OrdenEntregaForm_Load;
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button Salirbtn;
+        private Button GenerarOrdenEntregabtn;
+        private Label label2;
+        private GroupBox groupBox3;
+        private ListView Orden_Entrega;
+        private ColumnHeader CodOrdenColumna;
+        private ColumnHeader RazonClienteColumna;
+        private ColumnHeader TransportistaColumna;
+        private ColumnHeader fechaOPColumna;
+        private ColumnHeader fechaOEColumna;
+        private ColumnHeader OPColumna;
+    }
+}
---
+++ 5. GenerarOrdenEntrega/OrdenConfirmadas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Pampazon.OrdenEntrega
+{
+    internal class OrdenConfirmadas
+    {
+        internal class OrdenesConfirmadas
+        {
+            public int Nro_OrdenS { get; set; }
+            public DateTime FechaEmision { get; set; }
+            public int OP_Asociada { get; set; }
+            public string Estado { get; set; }
+            public DateTime Fecha_Estado { get; set; }
+            public List<Productos> Productos { get; set; }
+
+            public OrdenesConfirmadas(int nro_orden, DateTime fechaemision, int op_asociada, string estado, DateTime fecha_estado, List<Productos> productos)
+            {
+                Nro_OrdenS = nro_orden;
+                FechaEmision = fechaemision;
+                OP_Asociada = op_asociada;
+                Estado = estado;
+                Fecha_Estado = fecha_estado;
+                Productos = productos;
+            }
+           
+        }
+    }
+}
