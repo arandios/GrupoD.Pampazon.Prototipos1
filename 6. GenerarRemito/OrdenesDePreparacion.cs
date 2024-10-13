0b65fa9aeda6cf78a877299badaa36452c47fe94
EZEQUIEL MARCELO ARANDA <65AR43000399@campus.economicas.uba.ar>
2024-10-13 04:24:11 -0300

    ACT

--- 6. GenerarRemito/OrdenesDePreparacion.cs
+++ 6. GenerarRemito/OrdenesDePreparacion.cs
@@ -9,17 +9,17 @@ namespace Pampazon.Remitos
 {
     internal class OrdenesDePreparacion
     {
-        public string IdOrden { get; set; } 
-        public DateTime Fecha { get; set; } 
+       
+        public string IdOrden { get; set; }    
         public int DNItransportista { get; set; }
         public string EstadoDeOrden { get; set; } = "Preparadas";
 
         // Constructor
         public OrdenesDePreparacion(string idOrden, int dniTransportista)
         {
+          
             IdOrden = idOrden;
             DNItransportista = dniTransportista; 
-            Fecha = DateTime.Now.AddDays(new Random().Next(-10, 1)).Date;
         }
 
 
--- 6. GenerarRemito/Remito.cs
+++ 6. GenerarRemito/Remito.cs
@@ -8,20 +8,32 @@ namespace Pampazon.Remitos
 {
     internal class Remito
     {
+        // Propiedades
         public List<OrdenesDePreparacion> Ordenes { get; set; }
         public int DNITransportista { get; set; }
         public DateTime FechaDeEmision { get; set; }
+        public string IdRemito { get; private set; } // Propiedad para el ID del remito
+
+        // Campo estático para llevar el último ID generado
+        private static int _lastId = 0;
 
         // Constructor
-        public Remito (List<OrdenesDePreparacion> ordenes, int transportista)
+        public Remito(List<OrdenesDePreparacion> ordenes, int transportista)
         {
             Ordenes = ordenes;
             DNITransportista = transportista;
             FechaDeEmision = DateTime.Now;
+            IdRemito = GenerateId(); // Generar y asignar el nuevo ID al remito
         }
-        
 
+        // Método para generar un nuevo ID en el formato R-00000
+        private static string GenerateId()
+        {
+            _lastId++; // Incrementar el último ID
+            return $"R-{_lastId:D5}"; // Devolver el ID en el formato R-00000
+        }
     }
+
 
 }
 
