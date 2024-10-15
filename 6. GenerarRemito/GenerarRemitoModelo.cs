f42eba7c66aed4161b7ce5cb97a6dc37201f9283
EZEQUIEL MARCELO ARANDA <65AR43000399@campus.economicas.uba.ar>
2024-10-15 07:00:59 -0300

    Se elimina la clase transportista y se actualiza el metodo de busqueda

--- 6. GenerarRemito/GenerarRemitoModelo.cs
+++ 6. GenerarRemito/GenerarRemitoModelo.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 public class GenerarRemitoModelo
 {
     // Lista de transportistas
-    private static List<TransportistaRemito> transportistas = ObtenerTransportistas();
+   
 
     // Lista de órdenes de preparación
     private static List<OrdenesDePreparacion> ordenes = ObtenerOrdenes();
@@ -31,27 +31,31 @@ public class GenerarRemitoModelo
     // Método para verificar la existencia del transportista
          internal static bool ExisteTransportistaPorDni(int dni)
          {
-             return transportistas.Any(t => t.DNI == dni);
+            foreach (OrdenesDePreparacion orden in ordenes)
+            {
+                if (orden.DNItransportista == dni)
+                {
+                    return true;
+                }
+            } 
+            return false;
          }
 
-         // Método para obtener el nombre y apellido del transportista
-         internal static string ObtenerNombreTransportistaPorDni(int dni)
-         {
-             // Buscar el transportista en la lista usando el DNI
-             var transportista = transportistas.FirstOrDefault(t => t.DNI == dni);
-
-             // Si se encuentra el transportista, concatenar nombre y apellido
-             if (transportista != null)
-             {
-                 return $"{transportista.Nombre} {transportista.Apellido}";
-             }
-
-             // Si no se encuentra, devolver string vacío o null
+    // Método para obtener el nombre y apellido del transportista
+    internal static string ObtenerNombreTransportistaPorDni(int dni)
+    {
+        foreach (OrdenesDePreparacion orden in ordenes)
+        {
+            if (orden.DNItransportista == dni)
+            {
+                return $"{orden.NombreTransportista} " + $"{orden.ApellidoTransportista}";
+            }
+        }
              return string.Empty; // O puedes devolver null;
-         }
+    }
 
 
-    internal static List<OrdenesDePreparacion> ObtenerOrdenesDePreparacionPorDni(int dni)
+        internal static List<OrdenesDePreparacion> ObtenerOrdenesDePreparacionPorDni(int dni)
         {
             return ordenes.Where(o => o.DNItransportista == dni).ToList();
         }
@@ -74,52 +78,26 @@ public class GenerarRemitoModelo
 
 
 
-    internal static List<TransportistaRemito> ObtenerTransportistas()
-    {
-        return new List<TransportistaRemito>()
-        {
-            new TransportistaRemito(52983472, "Juan", "Perez"),
-            new TransportistaRemito(42938571, "Maria", "Lopez"),
-            new TransportistaRemito(75930281, "Carlos", "Martinez"),
-            new TransportistaRemito(63829472, "Mario", "Paz"),
-            new TransportistaRemito(13927485, "Ana", "Gomez"),
-            new TransportistaRemito(83920174, "Luis", "Diaz"), //prueba para transportista sin ordenes
-            new TransportistaRemito(40293857, "Marta", "Sanchez"),
-            new TransportistaRemito(94038271, "Sofia", "Ramos"),
-            new TransportistaRemito(50283741, "Laura", "Fernandez"),
-            new TransportistaRemito(73820194, "Daniel", "Cruz"),
-            new TransportistaRemito(12039475, "Pedro", "Alvarez"),
-            new TransportistaRemito(39284710, "Lucas", "Garcia"),//prueba para transportista sin ordenes
-            new TransportistaRemito(84930274, "Elena", "Molina") //prueba para transportista sin ordenes
-        };
-    }
+   
 
     internal static List<OrdenesDePreparacion> ObtenerOrdenes()
     {
         return new List<OrdenesDePreparacion>
         {
-            new OrdenesDePreparacion("OD_00123", 52983472),
-            new OrdenesDePreparacion("OD_00124", 42938571),
-            new OrdenesDePreparacion("OD_00125", 73928563),
-            new OrdenesDePreparacion("OD_00129", 40293857),
-            new OrdenesDePreparacion("OD_00130", 75930281),
-            new OrdenesDePreparacion("OD_00131", 50283741),
-            new OrdenesDePreparacion("OD_00132", 75930281),
-            new OrdenesDePreparacion("OD_00136", 23984720),
-            new OrdenesDePreparacion("OD_00137", 10394827),
-            new OrdenesDePreparacion("OD_00138", 75930281),
-            new OrdenesDePreparacion("OD_00139", 37492018),
-            new OrdenesDePreparacion("OD_00140", 29837465),
-            new OrdenesDePreparacion("OD_00141", 94832015),
-            new OrdenesDePreparacion("OD_00142", 75930281),
-            new OrdenesDePreparacion("OD_00144", 74029385),
-            new OrdenesDePreparacion("OD_00145", 29485710),
-            new OrdenesDePreparacion("OD_00146", 83920192),
-            new OrdenesDePreparacion("OD_00148", 75930281),
-            new OrdenesDePreparacion("OD_00149", 59203874),
-            new OrdenesDePreparacion("OD_00150", 23984720),
-            new OrdenesDePreparacion("OD_00151", 84920384),
-            new OrdenesDePreparacion("OD_00152", 94832015)
+            new OrdenesDePreparacion("OD_00123", 52983472, "Juan", "Perez"),
+            new OrdenesDePreparacion("OD_00124", 42938571, "Maria", "Lopez"),
+            new OrdenesDePreparacion("OD_00125", 73928563, "Carlos", "Martinez"),
+            new OrdenesDePreparacion("OD_00129", 40293857, "Mario", "Paz"),
+            new OrdenesDePreparacion("OD_00130", 75930281,"Ana", "Gomez" ),
+            new OrdenesDePreparacion("OD_00131", 50283741,"Luis", "Diaz" ),
+            new OrdenesDePreparacion("OD_00132", 75930281,"Ana", "Gomez" ),
+            new OrdenesDePreparacion("OD_00136", 23984720,"Sofia", "Ramos" ),
+            new OrdenesDePreparacion("OD_00137", 10394827,"Laura", "Fernandez" ),
+            new OrdenesDePreparacion("OD_00138", 75930281,"Ana", "Gomez" ),
+            new OrdenesDePreparacion("OD_00139", 37492018,"Pedro", "Alvarez" ),
+            new OrdenesDePreparacion("OD_00140", 29837465,"Lucas", "Garcia" ),
+            new OrdenesDePreparacion("OD_00141", 94832015,"Elena", "Molina" ),
+
         };
     }
 }
--- 6. GenerarRemito/OrdenesDePreparacion.cs
+++ 6. GenerarRemito/OrdenesDePreparacion.cs
@@ -12,14 +12,18 @@ namespace Pampazon.Remitos
        
         public string IdOrden { get; set; }    
         public int DNItransportista { get; set; }
+        public string NombreTransportista { get; set; }
+        public string ApellidoTransportista { get; set; }
         public string EstadoDeOrden { get; set; } = "Preparadas";
 
         // Constructor
-        public OrdenesDePreparacion(string idOrden, int dniTransportista)
+        public OrdenesDePreparacion(string idOrden, int dniTransportista, string Nombre, string Apellido)
         {
           
             IdOrden = idOrden;
             DNItransportista = dniTransportista; 
+            NombreTransportista = Nombre;
+            ApellidoTransportista = Apellido;
         }
 
 
