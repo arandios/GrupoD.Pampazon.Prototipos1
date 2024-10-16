f2f2558daa1394f8aadb1048af5de954152c60d3
TOMAS IGNACIO SAMPIETRO <08SA42645963@campus.economicas.uba.ar>
2024-10-15 23:14:19 -0300

    avances

---
+++ 3. BuscarProductosEnDepositos/OrdenDeSeleccion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pampazon.BuscarProductosEnDepositos
+{
+    internal class OrdenDeSeleccion
+    {
+        public int IDOrdenSeleccion { get; set; }
+    }
+}
---
+++ 3. BuscarProductosEnDepositos/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pampazon.BuscarProductosEnDepositos
+{
+    internal class Producto
+    {
+        public string Ubicacion { get; set; }
+        public string SKU { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
