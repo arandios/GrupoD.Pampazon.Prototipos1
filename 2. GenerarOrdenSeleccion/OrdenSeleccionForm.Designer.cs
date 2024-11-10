f9a00fcc84905642a161a1df5c58dfbe151ed488
AlanPrado03 <pradoalan47@gmail.com>
2024-11-09 23:53:04 -0300

    Casos de prueba 5

--- 2. GenerarOrdenSeleccion/OrdenSeleccionForm.Designer.cs
+++ 2. GenerarOrdenSeleccion/OrdenSeleccionForm.Designer.cs
@@ -68,10 +68,9 @@
             // QuitarOrdenPreparacionASeleccionBTN
             // 
             QuitarOrdenPreparacionASeleccionBTN.BackColor = Color.FromArgb(255, 192, 192);
-            QuitarOrdenPreparacionASeleccionBTN.Location = new Point(637, 207);
-            QuitarOrdenPreparacionASeleccionBTN.Margin = new Padding(3, 2, 3, 2);
+            QuitarOrdenPreparacionASeleccionBTN.Location = new Point(728, 276);
             QuitarOrdenPreparacionASeleccionBTN.Name = "QuitarOrdenPreparacionASeleccionBTN";
-            QuitarOrdenPreparacionASeleccionBTN.Size = new Size(197, 41);
+            QuitarOrdenPreparacionASeleccionBTN.Size = new Size(225, 55);
             QuitarOrdenPreparacionASeleccionBTN.TabIndex = 0;
             QuitarOrdenPreparacionASeleccionBTN.Text = "&Quitar Orden de Preparación";
             QuitarOrdenPreparacionASeleccionBTN.UseVisualStyleBackColor = false;
@@ -79,10 +78,9 @@
             // 
             // AgregarOrdenSeleccionADetalleBTN
             // 
-            AgregarOrdenSeleccionADetalleBTN.Location = new Point(625, 190);
-            AgregarOrdenSeleccionADetalleBTN.Margin = new Padding(3, 2, 3, 2);
+            AgregarOrdenSeleccionADetalleBTN.Location = new Point(714, 253);
             AgregarOrdenSeleccionADetalleBTN.Name = "AgregarOrdenSeleccionADetalleBTN";
-            AgregarOrdenSeleccionADetalleBTN.Size = new Size(205, 40);
+            AgregarOrdenSeleccionADetalleBTN.Size = new Size(234, 53);
             AgregarOrdenSeleccionADetalleBTN.TabIndex = 1;
             AgregarOrdenSeleccionADetalleBTN.Text = "&Agregar al detalle de seleccion";
             AgregarOrdenSeleccionADetalleBTN.UseVisualStyleBackColor = true;
@@ -90,10 +88,9 @@
             // 
             // BuscarOrdenSeleccionBTN
             // 
-            BuscarOrdenSeleccionBTN.Location = new Point(448, 83);
-            BuscarOrdenSeleccionBTN.Margin = new Padding(3, 2, 3, 2);
+            BuscarOrdenSeleccionBTN.Location = new Point(512, 111);
             BuscarOrdenSeleccionBTN.Name = "BuscarOrdenSeleccionBTN";
-            BuscarOrdenSeleccionBTN.Size = new Size(196, 40);
+            BuscarOrdenSeleccionBTN.Size = new Size(224, 53);
             BuscarOrdenSeleccionBTN.TabIndex = 3;
             BuscarOrdenSeleccionBTN.Text = "&Buscar Ordenes de Preparación";
             BuscarOrdenSeleccionBTN.UseVisualStyleBackColor = true;
@@ -101,10 +98,9 @@
             // 
             // BorrarFiltrosOrdenSeleccionBTN
             // 
-            BorrarFiltrosOrdenSeleccionBTN.Location = new Point(649, 83);
-            BorrarFiltrosOrdenSeleccionBTN.Margin = new Padding(3, 2, 3, 2);
+            BorrarFiltrosOrdenSeleccionBTN.Location = new Point(742, 111);
             BorrarFiltrosOrdenSeleccionBTN.Name = "BorrarFiltrosOrdenSeleccionBTN";
-            BorrarFiltrosOrdenSeleccionBTN.Size = new Size(187, 40);
+            BorrarFiltrosOrdenSeleccionBTN.Size = new Size(214, 53);
             BorrarFiltrosOrdenSeleccionBTN.TabIndex = 2;
             BorrarFiltrosOrdenSeleccionBTN.Text = "&Borrar filtros";
             BorrarFiltrosOrdenSeleccionBTN.UseVisualStyleBackColor = true;
@@ -113,86 +109,83 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(13, 19);
+            label1.Location = new Point(15, 25);
             label1.Name = "label1";
-            label1.Size = new Size(0, 15);
+            label1.Size = new Size(0, 20);
             label1.TabIndex = 5;
             // 
             // FechaMinimaLBL
             // 
             FechaMinimaLBL.AutoSize = true;
-            FechaMinimaLBL.Location = new Point(204, 30);
+            FechaMinimaLBL.Location = new Point(233, 40);
             FechaMinimaLBL.Name = "FechaMinimaLBL";
-            FechaMinimaLBL.Size = new Size(82, 15);
+            FechaMinimaLBL.Size = new Size(101, 20);
             FechaMinimaLBL.TabIndex = 6;
             FechaMinimaLBL.Text = "Fecha Mínima";
             // 
             // PrioridadLBL
             // 
             PrioridadLBL.AutoSize = true;
-            PrioridadLBL.Location = new Point(204, 80);
+            PrioridadLBL.Location = new Point(233, 107);
             PrioridadLBL.Name = "PrioridadLBL";
-            PrioridadLBL.Size = new Size(55, 15);
+            PrioridadLBL.Size = new Size(70, 20);
             PrioridadLBL.TabIndex = 7;
             PrioridadLBL.Text = "Prioridad";
             // 
             // RazonSocialClienteLBL
             // 
             RazonSocialClienteLBL.AutoSize = true;
-            RazonSocialClienteLBL.Location = new Point(13, 45);
+            RazonSocialClienteLBL.Location = new Point(15, 60);
             RazonSocialClienteLBL.Name = "RazonSocialClienteLBL";
-            RazonSocialClienteLBL.Size = new Size(112, 15);
+            RazonSocialClienteLBL.Size = new Size(142, 20);
             RazonSocialClienteLBL.TabIndex = 8;
             RazonSocialClienteLBL.Text = "Razón social Cliente";
             // 
             // IdOrdenLBL
             // 
             IdOrdenLBL.AutoSize = true;
-            IdOrdenLBL.Location = new Point(0, 80);
+            IdOrdenLBL.Location = new Point(0, 107);
             IdOrdenLBL.Name = "IdOrdenLBL";
-            IdOrdenLBL.Size = new Size(136, 15);
+            IdOrdenLBL.Size = new Size(173, 20);
             IdOrdenLBL.TabIndex = 9;
             IdOrdenLBL.Text = "ID de Orden Preparación";
             // 
             // ClienteOrdenSeleccionTXT
             // 
-            ClienteOrdenSeleccionTXT.Location = new Point(13, 62);
-            ClienteOrdenSeleccionTXT.Margin = new Padding(3, 2, 3, 2);
+            ClienteOrdenSeleccionTXT.Location = new Point(15, 83);
             ClienteOrdenSeleccionTXT.Name = "ClienteOrdenSeleccionTXT";
-            ClienteOrdenSeleccionTXT.Size = new Size(196, 23);
+            ClienteOrdenSeleccionTXT.Size = new Size(223, 27);
             ClienteOrdenSeleccionTXT.TabIndex = 10;
             // 
             // NumeroOrdenPreparacionOrdenSeleccionTXT
             // 
-            NumeroOrdenPreparacionOrdenSeleccionTXT.Location = new Point(0, 98);
-            NumeroOrdenPreparacionOrdenSeleccionTXT.Margin = new Padding(3, 2, 3, 2);
+            NumeroOrdenPreparacionOrdenSeleccionTXT.Location = new Point(0, 131);
             NumeroOrdenPreparacionOrdenSeleccionTXT.Name = "NumeroOrdenPreparacionOrdenSeleccionTXT";
-            NumeroOrdenPreparacionOrdenSeleccionTXT.Size = new Size(199, 23);
+            NumeroOrdenPreparacionOrdenSeleccionTXT.Size = new Size(227, 27);
             NumeroOrdenPreparacionOrdenSeleccionTXT.TabIndex = 12;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(13, 127);
+            label6.Location = new Point(15, 169);
             label6.Name = "label6";
-            label6.Size = new Size(0, 15);
+            label6.Size = new Size(0, 20);
             label6.TabIndex = 14;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(13, 332);
+            label7.Location = new Point(15, 443);
             label7.Name = "label7";
-            label7.Size = new Size(0, 15);
+            label7.Size = new Size(0, 20);
             label7.TabIndex = 16;
             // 
             // CrearOrdenSeleccionBTN
             // 
             CrearOrdenSeleccionBTN.BackColor = Color.FromArgb(192, 255, 192);
-            CrearOrdenSeleccionBTN.Location = new Point(402, 666);
-            CrearOrdenSeleccionBTN.Margin = new Padding(3, 2, 3, 2);
+            CrearOrdenSeleccionBTN.Location = new Point(459, 888);
             CrearOrdenSeleccionBTN.Name = "CrearOrdenSeleccionBTN";
-            CrearOrdenSeleccionBTN.Size = new Size(245, 40);
+            CrearOrdenSeleccionBTN.Size = new Size(280, 53);
             CrearOrdenSeleccionBTN.TabIndex = 17;
             CrearOrdenSeleccionBTN.Text = "&Generar Orden de Selección";
             CrearOrdenSeleccionBTN.UseVisualStyleBackColor = false;
@@ -200,10 +193,9 @@
             // 
             // CancelarOrdenSeleccionBTN
             // 
-            CancelarOrdenSeleccionBTN.Location = new Point(652, 666);
-            CancelarOrdenSeleccionBTN.Margin = new Padding(3, 2, 3, 2);
+            CancelarOrdenSeleccionBTN.Location = new Point(745, 888);
             CancelarOrdenSeleccionBTN.Name = "CancelarOrdenSeleccionBTN";
-            CancelarOrdenSeleccionBTN.Size = new Size(187, 40);
+            CancelarOrdenSeleccionBTN.Size = new Size(214, 53);
             CancelarOrdenSeleccionBTN.TabIndex = 18;
             CancelarOrdenSeleccionBTN.Text = "&Salir";
             CancelarOrdenSeleccionBTN.UseVisualStyleBackColor = true;
@@ -221,11 +213,9 @@
             FiltrosOPGRP.Controls.Add(PrioridadOrdenSeleccionCMB);
             FiltrosOPGRP.Controls.Add(BuscarOrdenSeleccionBTN);
             FiltrosOPGRP.Controls.Add(BorrarFiltrosOrdenSeleccionBTN);
-            FiltrosOPGRP.Location = new Point(10, 15);
-            FiltrosOPGRP.Margin = new Padding(3, 2, 3, 2);
+            FiltrosOPGRP.Location = new Point(11, 20);
             FiltrosOPGRP.Name = "FiltrosOPGRP";
-            FiltrosOPGRP.Padding = new Padding(3, 2, 3, 2);
-            FiltrosOPGRP.Size = new Size(839, 127);
+            FiltrosOPGRP.Size = new Size(959, 169);
             FiltrosOPGRP.TabIndex = 19;
             FiltrosOPGRP.TabStop = false;
             FiltrosOPGRP.Text = "Seleccione filtros de búsqueda para Ordenes de Preparación: ";
@@ -233,47 +223,43 @@
             // FechaMaximaLBL
             // 
             FechaMaximaLBL.AutoSize = true;
-            FechaMaximaLBL.Location = new Point(519, 30);
+            FechaMaximaLBL.Location = new Point(593, 40);
             FechaMaximaLBL.Name = "FechaMaximaLBL";
-            FechaMaximaLBL.Size = new Size(84, 15);
+            FechaMaximaLBL.Size = new Size(104, 20);
             FechaMaximaLBL.TabIndex = 25;
             FechaMaximaLBL.Text = "Fecha Máxima";
             // 
             // FechaFinalOrdenSeleccionDTP
             // 
-            FechaFinalOrdenSeleccionDTP.Location = new Point(519, 47);
-            FechaFinalOrdenSeleccionDTP.Margin = new Padding(3, 2, 3, 2);
+            FechaFinalOrdenSeleccionDTP.Location = new Point(593, 63);
             FechaFinalOrdenSeleccionDTP.Name = "FechaFinalOrdenSeleccionDTP";
-            FechaFinalOrdenSeleccionDTP.Size = new Size(310, 23);
+            FechaFinalOrdenSeleccionDTP.Size = new Size(354, 27);
             FechaFinalOrdenSeleccionDTP.TabIndex = 24;
             // 
             // FechaInicioOrdenSeleccionDTP
             // 
-            FechaInicioOrdenSeleccionDTP.Location = new Point(204, 47);
-            FechaInicioOrdenSeleccionDTP.Margin = new Padding(3, 2, 3, 2);
+            FechaInicioOrdenSeleccionDTP.Location = new Point(233, 63);
             FechaInicioOrdenSeleccionDTP.Name = "FechaInicioOrdenSeleccionDTP";
-            FechaInicioOrdenSeleccionDTP.Size = new Size(310, 23);
+            FechaInicioOrdenSeleccionDTP.Size = new Size(354, 27);
             FechaInicioOrdenSeleccionDTP.TabIndex = 1;
             // 
             // PrioridadOrdenSeleccionCMB
             // 
             PrioridadOrdenSeleccionCMB.DropDownStyle = ComboBoxStyle.DropDownList;
             PrioridadOrdenSeleccionCMB.FormattingEnabled = true;
-            PrioridadOrdenSeleccionCMB.Location = new Point(204, 98);
-            PrioridadOrdenSeleccionCMB.Margin = new Padding(3, 2, 3, 2);
+            PrioridadOrdenSeleccionCMB.Location = new Point(233, 131);
             PrioridadOrdenSeleccionCMB.Name = "PrioridadOrdenSeleccionCMB";
-            PrioridadOrdenSeleccionCMB.Size = new Size(237, 23);
+            PrioridadOrdenSeleccionCMB.Size = new Size(270, 28);
             PrioridadOrdenSeleccionCMB.TabIndex = 0;
+            PrioridadOrdenSeleccionCMB.SelectedIndexChanged += PrioridadOrdenSeleccionCMB_SelectedIndexChanged;
             // 
             // OPGRP
             // 
             OPGRP.Controls.Add(OrdenesDePrepracionAOrdenSeleccionLST);
             OPGRP.Controls.Add(AgregarOrdenSeleccionADetalleBTN);
-            OPGRP.Location = new Point(10, 162);
-            OPGRP.Margin = new Padding(3, 2, 3, 2);
+            OPGRP.Location = new Point(11, 216);
             OPGRP.Name = "OPGRP";
-            OPGRP.Padding = new Padding(3, 2, 3, 2);
-            OPGRP.Size = new Size(839, 240);
+            OPGRP.Size = new Size(959, 320);
             OPGRP.TabIndex = 20;
             OPGRP.TabStop = false;
             OPGRP.Text = "Ordenes de Preparación en estado Pendiente: ";
@@ -281,10 +267,9 @@
             // OrdenesDePrepracionAOrdenSeleccionLST
             // 
             OrdenesDePrepracionAOrdenSeleccionLST.Columns.AddRange(new ColumnHeader[] { IDOrdenPreparacionColumna, RSCliente_Columna, FechaOrdenPreparacionColumna, PrioridadColumna });
-            OrdenesDePrepracionAOrdenSeleccionLST.Location = new Point(4, 20);
-            OrdenesDePrepracionAOrdenSeleccionLST.Margin = new Padding(3, 2, 3, 2);
+            OrdenesDePrepracionAOrdenSeleccionLST.Location = new Point(5, 27);
             OrdenesDePrepracionAOrdenSeleccionLST.Name = "OrdenesDePrepracionAOrdenSeleccionLST";
-            OrdenesDePrepracionAOrdenSeleccionLST.Size = new Size(829, 167);
+            OrdenesDePrepracionAOrdenSeleccionLST.Size = new Size(947, 221);
             OrdenesDePrepracionAOrdenSeleccionLST.TabIndex = 2;
             OrdenesDePrepracionAOrdenSeleccionLST.UseCompatibleStateImageBehavior = false;
             OrdenesDePrepracionAOrdenSeleccionLST.View = View.Details;
@@ -314,11 +299,9 @@
             OSGRP.Controls.Add(DetalleOrdenesDePreparacionPendientesLST);
             OSGRP.Controls.Add(QuitarOrdenPreparacionASeleccionBTN);
             OSGRP.Enabled = false;
-            OSGRP.Location = new Point(10, 406);
-            OSGRP.Margin = new Padding(3, 2, 3, 2);
+            OSGRP.Location = new Point(11, 541);
             OSGRP.Name = "OSGRP";
-            OSGRP.Padding = new Padding(3, 2, 3, 2);
-            OSGRP.Size = new Size(839, 254);
+            OSGRP.Size = new Size(959, 339);
             OSGRP.TabIndex = 21;
             OSGRP.TabStop = false;
             OSGRP.Text = "Detalle de Ordenes de Preparación a insertar: ";
@@ -326,10 +309,9 @@
             // DetalleOrdenesDePreparacionPendientesLST
             // 
             DetalleOrdenesDePreparacionPendientesLST.Columns.AddRange(new ColumnHeader[] { OPaOS_IdOPColumna, OPaOS_RazonSocialClienteColumna, OPaOS_FechaOPColumna, OPaOS_PrioridadColumna });
-            DetalleOrdenesDePreparacionPendientesLST.Location = new Point(5, 32);
-            DetalleOrdenesDePreparacionPendientesLST.Margin = new Padding(3, 2, 3, 2);
+            DetalleOrdenesDePreparacionPendientesLST.Location = new Point(6, 43);
             DetalleOrdenesDePreparacionPendientesLST.Name = "DetalleOrdenesDePreparacionPendientesLST";
-            DetalleOrdenesDePreparacionPendientesLST.Size = new Size(829, 163);
+            DetalleOrdenesDePreparacionPendientesLST.Size = new Size(947, 216);
             DetalleOrdenesDePreparacionPendientesLST.TabIndex = 4;
             DetalleOrdenesDePreparacionPendientesLST.UseCompatibleStateImageBehavior = false;
             DetalleOrdenesDePreparacionPendientesLST.View = View.Details;
@@ -356,9 +338,9 @@
             // 
             // OrdenSeleccionForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(859, 715);
+            ClientSize = new Size(982, 953);
             Controls.Add(CancelarOrdenSeleccionBTN);
             Controls.Add(CrearOrdenSeleccionBTN);
             Controls.Add(label7);
@@ -369,7 +351,6 @@
             Controls.Add(FiltrosOPGRP);
             Controls.Add(OPGRP);
             Controls.Add(OSGRP);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "OrdenSeleccionForm";
             Text = "Generar Orden Seleccion";
             Load += OrdenSeleccionForm_Load_1;
